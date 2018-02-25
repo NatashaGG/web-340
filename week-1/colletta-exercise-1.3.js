079057d93e33ea985ba12c50b1734b796e962a47
Natasha C <natashacolletta@gmail.com>
2018-02-24 18:39:26 -0600

    updated header

--- week-1/colletta-exercise-1.3.js
+++ week-1/colletta-exercise-1.3.js
@@ -1,3 +1,17 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Professor Krasso
+; Date: 24 Feb 2018
+; Modified By: Natasha Colletta
+; Description: creates a url variable to be
+; parsed out into sections.
+;===========================================
+*/ 
+var header = require('../header.js');
+var headerMsg = header.display("Natasha", "Colletta", "Exercise 1.3");
+console.log(headerMsg + "\n");
+
 var url = require('url');
 
 var parsedURL = url.parse('https://www.reddit.com/subreddit?cats');
