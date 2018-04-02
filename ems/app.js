55db07eb4f3340be29bd8c367bb30cbf59c8f53c
Natasha Colletta <ncolletta@bellevue.edu>
2018-04-01 22:05:51 -0500

    Uploaded Views and ems files

---
+++ ems/app.js
@@ -0,0 +1,19 @@
+// Colletta's App.js
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+app.set("views".path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(logger("short"));
+app.get("/", function(req, res){
+    res.render("index", {
+        title: "Home page"
+    });
+});
+
+http.createServer(app).listen(8080, function(){
+    console.log("Applications has started and is listening on port 8080!");
+});
